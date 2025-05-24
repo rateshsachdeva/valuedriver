bd07eac5f582e8972534310664cbc1ab92d4fd35
rateshsachdeva <rateshsachdeva@gmail.com>
2025-05-24 20:45:20 +0530

    Update suggested-actions.tsx

--- components/suggested-actions.tsx
+++ components/suggested-actions.tsx
@@ -40,42 +40,44 @@ function PureSuggestedActions({
     },
   ];
 
-  return (
-    <div
-      data-testid="suggested-actions"
-      className="grid sm:grid-cols-2 gap-2 w-full"
-    >
-      {suggestedActions.map((suggestedAction, index) => (
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: 20 }}
-          transition={{ delay: 0.05 * index }}
-          key={`suggested-action-${suggestedAction.title}-${index}`}
-          className={index > 1 ? 'hidden sm:block' : 'block'}
-        >
-          <Button
-            variant="ghost"
-            onClick={async () => {
-              window.history.replaceState({}, '', `/chat/${chatId}`);
+// return (
+//   <div
+//     data-testid="suggested-actions"
+//     className="grid sm:grid-cols-2 gap-2 w-full"
+//   >
+//     {suggestedActions.map((suggestedAction, index) => (
+//       <motion.div
+//         initial={{ opacity: 0, y: 20 }}
+//         animate={{ opacity: 1, y: 0 }}
+//         exit={{ opacity: 0, y: 20 }}
+//         transition={{ delay: 0.05 * index }}
+//         key={`suggested-action-${suggestedAction.title}-${index}`}
+//         className={index > 1 ? 'hidden sm:block' : 'block'}
+//       >
+//         <Button
+//           variant="ghost"
+//           onClick={async () => {
+//             window.history.replaceState({}, '', `/chat/${chatId}`);
 
-              append({
-                role: 'user',
-                content: suggestedAction.action,
-              });
-            }}
-            className="text-left border rounded-xl px-4 py-3.5 text-sm flex-1 gap-1 sm:flex-col w-full h-auto justify-start items-start"
-          >
-            <span className="font-medium">{suggestedAction.title}</span>
-            <span className="text-muted-foreground">
-              {suggestedAction.label}
-            </span>
-          </Button>
-        </motion.div>
-      ))}
-    </div>
-  );
-}
+//             append({
+//               role: 'user',
+//               content: suggestedAction.action,
+//             });
+//           }}
+//           className="text-left border rounded-xl px-4 py-3.5 text-sm flex-1 gap-1 sm:flex-col w-full h-auto justify-start items-start"
+//         >
+//           <span className="font-medium">{suggestedAction.title}</span>
+//           <span className="text-muted-foreground">
+//             {suggestedAction.label}
+//           </span>
+//         </Button>
+//       </motion.div>
+//     ))}
+//   </div>
+// );
+
+return null;
+
 
 export const SuggestedActions = memo(
   PureSuggestedActions,
